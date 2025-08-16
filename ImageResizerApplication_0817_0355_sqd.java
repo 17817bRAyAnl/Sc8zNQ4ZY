61b9eee71c497a246992118161c10dc34dfaa485
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-08-16 20:56:00 +0100

    Configure 图片尺寸批量调整器 using SPRINGBOOT

---
+++ ImageResizerApplication_0817_0355_sqd.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-08-17 03:55:59
+package com.example.imageresizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageResizerApplication.class, args);
+    }
+}
