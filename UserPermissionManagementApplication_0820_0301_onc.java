209468ca5844a1ed85dc3876bb8de5d25a9fa579
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-08-19 20:01:37 +0100

    Setup 用户权限管理系统

---
+++ UserPermissionManagementApplication_0820_0301_onc.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-08-20 03:01:36
+package com.example.userpermissionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPermissionManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserPermissionManagementApplication.class, args);
+    }
+}
