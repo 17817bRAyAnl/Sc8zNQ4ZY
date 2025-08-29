d93114b4b8b14c86b7d26fd627dd900e17e4177c
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-08-29 19:11:31 +0100

    Configure 用户界面组件库

---
+++ userInterfaceLibraryApplication_0830_0211_taf.java
@@ -0,0 +1,27 @@
+// 代码生成时间: 2025-08-30 02:11:30
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class UserInterfaceLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserInterfaceLibraryApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
