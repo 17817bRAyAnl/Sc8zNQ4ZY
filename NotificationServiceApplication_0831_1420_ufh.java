536c9f60784677db3b3978866db3f2de92f04862
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-08-31 07:20:39 +0100

    Add JAVA 消息通知系统

---
+++ NotificationServiceApplication_0831_1420_ufh.java
@@ -0,0 +1,14 @@
+// 代码生成时间: 2025-08-31 14:20:38
+package com.example.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+# 优化算法效率
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
