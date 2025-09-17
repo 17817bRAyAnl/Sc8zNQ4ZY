917d0d3d1bedd715468e9c4429bcffda08229694
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-09-17 22:04:22 +0100

    Update 随机数生成器 using SPRINGBOOT

---
+++ RandomNumberGeneratorApplication_0918_0504_oqn.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-09-18 05:04:20
+package com.example.randomnumbergenerator;\
+\
+import org.springframework.boot.SpringApplication;\
+import org.springframework.boot.autoconfigure.SpringBootApplication;\
+\
+@SpringBootApplication\
+public class RandomNumberGeneratorApplication {\
+    public static void main(String[] args) {\
+        SpringApplication.run(RandomNumberGeneratorApplication.class, args);\
+    }\
+}\
