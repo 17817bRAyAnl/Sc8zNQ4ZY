6e0ed2532f6a82f8ae380cfcbc02e6b49862cfd8
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-09-18 03:51:43 +0100

    Develop 批量文件重命名工具 using SPRINGBOOT

---
+++ BatchFileRenamerApplication_0918_1051_cpt.java
@@ -0,0 +1,14 @@
+// 代码生成时间: 2025-09-18 10:51:42
+package com.example.batchfilerenamer;
+
+import org.springframework.boot.SpringApplication;
+# 优化算法效率
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+# FIXME: 处理边界情况
+public class BatchFileRenamerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchFileRenamerApplication.class, args);
+    }
+}
