e44545dd7af1ee98a7247a69d96c8c29835a5801
17817bRAyAnl <hobbits_barrage_0b@icloud.com>
2025-10-09 17:00:21 +0100

    Implement JAVA 虚拟实验室

---
+++ VirtualLabApplication_1010_0000_dxu.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-10-10 00:00:16
+package com.virtuallab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualLabApplication.class, args);
+    }
+}
